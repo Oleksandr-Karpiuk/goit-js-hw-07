ae61c20d47cae795870da5ddec283c2ed052a9dd
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-02-23 13:23:40 +0200

    Додає завершене 1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryListRef = document.querySelector("div.gallery");
+
+const makeGalleryListMarkup = ({ preview, original, description }) => {
+  return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+      </a>
+    </div>
+  `;
+};
+
+const makeGalleryListElement = galleryItems.map(makeGalleryListMarkup).join("");
+galleryListRef.insertAdjacentHTML("beforeend", makeGalleryListElement);
+
+galleryListRef.addEventListener("click", onGalleryImageClick);
+
+function onGalleryImageClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  galleryListRef.addEventListener("keydown", onEscapeClose);
+  function onEscapeClose(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+      galleryListRef.removeEventListener("keydown", onEscapeClose);
+    }
+  }
+}
