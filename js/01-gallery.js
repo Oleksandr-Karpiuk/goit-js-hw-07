dea292dc77f0a9398970721f11e79a8d5c85947a
Oleksandr Karpiuk <alex.karpuyk@gmail.com>
2023-02-25 18:24:06 +0200

    Оновлює 1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -29,19 +29,22 @@ function onGalleryImageClick(evt) {
     return;
   }
 
-  const instance = basicLightbox.create(`
-    <img src="${evt.target.dataset.source}" width="800" height="600">
-  `);
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        galleryListRef.addEventListener("keydown", onEscapeClose);
+      },
+      onClose: (instance) => {
+        galleryListRef.removeEventListener("keydown", onEscapeClose);
+      },
+    }
+  );
 
   instance.show();
 
-  galleryListRef.addEventListener("keydown", onEscapeClose, { once: true });
-
-  onClose: () => {
-    document.removeEventListener("keydown", onEscapeClose);
-  };
-
   function onEscapeClose(evt) {
+    console.log(evt);
     if (evt.code === "Escape") {
       instance.close();
     }
